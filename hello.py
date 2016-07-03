e1c891b7495bd2c68b25d088afb55c976e78597c
manikandaraj@yahoo.com <manikandaraj@yahoo.com>
2016-07-02 19:12:23 -0700

    routes

--- hello.py
+++ hello.py
@@ -1,11 +1,28 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 
-def hello_world():
-	return 'hello world'
+def index():
+	return "Index page"
+
+@app.route('/hello')
+
+def hello():
+	return "Hello World"
+
+@app.route('/user/<username>')
+
+def printuser(username):
+	return "Hello" + " " + username
+
+@app.route('/post/<int:postid>')
+
+def printpost(postid):
+	return "Hello %d" % postid
 
 if __name__ == '__main__':
+	app.debug = True
 	app.run()
