d18720607fc253e23fb1d1c2e3472e928021fa25
manikandaraj@yahoo.com <manikandaraj@yahoo.com>
2016-07-07 08:10:38 -0700

    login function

--- hello.py
+++ hello.py
@@ -6,9 +6,20 @@ app = Flask(__name__)
 @app.route('/logins',methods=['GET','POST'])
 
 def login():
+	error = "None"
 	if request.method == 'POST':
-		return "User %s logged in " % request.form['username']
+		if valid_login(request.form.get('username'),request.form.get('password')):
+			return "Welcome back %s " % request.form['username']
+		else:
+			error = "Incorrect username and pwd"
+			return render_template('login.html',error = error)
 	return render_template("login.html")
+
+def valid_login(username,password):
+	if  username == password:
+		return True
+	else:
+		return False
 
 if __name__ == '__main__':
 	app.debug = True
